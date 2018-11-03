83c02a9e808f8e021f3ec5bee640c0fd7a2b4fe9
SamsonovDima <dimka0somik@gmail.com>
2018-11-03 21:16:43 +0300

    Add files via upload

--- js/xml.js
+++ js/xml.js
@@ -7,33 +7,33 @@ request.onload = function() {
     var CityList = request.response;
     outCity(CityList);
 }
-
 function outCity(CityList) {
-
-    // document.body.onclick = function(event) { // поменять на что то другое 
-    //     event = event || window.event;
-    function change(){
+    function change() {
         var select = document.getElementsByTagName('select');
-        for (var y=0; y < select.length; y++){
-            select[y].onchange = function(e){
-                console.log(e) // перенестии  сюда кусок кода
+        for (var y = 0; y < select.length; y++) {
+            select[y].onchange = function(e) {
+                // console.log(e) // перенестии  сюда кусок кода
 
                 var out = '';
                 for (var i in CityList) {
                     var cantry = event.target.value
-                    console.log(cantry)
+                    // console.log(cantry)
                     if (CityList[i].country == cantry) {
-                        out += "<p data-id=" + CityList[i].id + " class=citylocation >" + CityList[i].name + "</p>"
+                        out += "<option data-val=" + CityList[i].id + " class=citylocation >" + CityList[i].name + "</option>"
                         var outCity = document.getElementById('out');
                         outCity.innerHTML = out
-                        console.log(CityList[i].country)
-        
+                        // console.log(CityList[i].country)
+
                         document.body.onclick = function(event) {
                             event = event || window.event;
-        
-                            if (event.target.className == 'citylocation') {
-                                var dataId = event.target.dataset.id;
-                                // console.log(dataId)
+                            console.log(event)
+                            var dataclass = event.target.className;
+                            console.log(dataclass)
+                            if (dataclass == 'citylocation') {
+                                // var dataId = event.target.attributes.id.ownerElement.selectedOptions[0].dataset.val;
+                                // var dataId = event.target.attributes[0].value;
+                                var dataId = event.target.dataset.val;
+                                console.log(dataId)
                                 var xhr = new XMLHttpRequest();
                                 xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + dataId + '&appid=c4ecaf46d9687ed18d60681ab359b68f', false);
                                 xhr.send();
@@ -42,23 +42,23 @@ function outCity(CityList) {
                                     alert(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
                                 } else {
                                     // вывести результат
-                                    console.log(xhr.response); // responseText -- текст ответа.
+                                    // console.log(xhr.response); // responseText -- текст ответа.
                                     var retOut = JSON.parse(xhr.response);
                                     var out = '';
                                     out += "<h2> Temp: " + retOut.main.temp + " </h2>"
-                                    out +='<img src="https://openweathermap.org/img/w/'+ retOut.weather[0].icon +'.png">'
+                                    out += '<img src="https://openweathermap.org/img/w/' + retOut.weather[0].icon + '.png">'
                                     var outTemp = document.getElementById('outtemp')
                                     outTemp.innerHTML = out
-                                    console.log(retOut)
-                                    console.log(out)
-                                
-                            
-        
+                                    // console.log(retOut)
+                                    // console.log(out)
+
+
+
+                                }
+                            }
                         }
                     }
                 }
-                }
-            }
             }
         }
     }
